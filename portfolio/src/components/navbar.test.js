c85b96e2d7d3df4ca577985b564e4151f727310b
Anish <anishkakaiya@outlook.com>
2022-07-24 23:26:55 +0100

    unfinished test

---
+++ portfolio/src/components/navbar.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from './Navbar';
+
+it('Renders Navbar', () => {
+  render(<Navbar />);
+  const aboutMe = screen.getByText(/About Me/i);
+  const experience = screen.getByText(/Experience/i);
+  const projects = screen.getByText(/Projects/i);
+  const techStacks = screen.getByText(/Tech Stacks/i);
+  const contact = screen.getByText(/Contact/i);
+});
