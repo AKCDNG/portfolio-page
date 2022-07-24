13baa7db10959724a48b7b05c9185c4a3ef689e1
Anish <anishkakaiya@outlook.com>
2022-07-25 00:00:01 +0100

    add navbar test

--- portfolio/src/App.test.js
+++ portfolio/src/App.test.js
@@ -6,3 +6,19 @@ it('renders heading on page', () => {
   const linkElement = screen.getByRole('heading');
   expect(linkElement).toHaveTextContent('Welcome To My Personal Portfolio!');
 });
+
+it('renders NavBar on page', () => {
+  render(<App />);
+
+  const aboutMe = screen.getByText(/About Me/i);
+  const experience = screen.getByText(/Experience/i);
+  const projects = screen.getByText(/Projects/i);
+  const techStacks = screen.getByText(/Tech Stacks/i);
+  const contact = screen.getByText(/Contact/i);
+
+  expect(aboutMe).toBeInTheDocument();
+  expect(experience).toBeInTheDocument();
+  expect(projects).toBeInTheDocument();
+  expect(techStacks).toBeInTheDocument();
+  expect(contact).toBeInTheDocument();
+})
