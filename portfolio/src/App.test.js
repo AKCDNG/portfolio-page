4fc68a8aa545b2c2f62cd6358476506a90475c8a
Anish <anishkakaiya@outlook.com>
2022-07-24 22:03:04 +0100

    linting

--- portfolio/src/App.test.js
+++ portfolio/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders heading on page', () => {
+it('renders heading on page', () => {
   render(<App />);
   const linkElement = screen.getByRole('heading');
   expect(linkElement).toHaveTextContent('Welcome To My Personal Portfolio!');
