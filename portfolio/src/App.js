ca0a476fb20fe929bc83a49a2d215628eb8f34d3
Anish <anishkakaiya@outlook.com>
2022-07-25 00:00:14 +0100

    pass navbar test

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './index.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1 className="titletest">Welcome To My Personal Portfolio!</h1>
+      <Navbar />
     </div>
   );
 }
