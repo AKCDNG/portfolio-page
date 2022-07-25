d00a7bc8fc47e1dd5f71ed9e57fde607338a0005
Anish <anishkakaiya@outlook.com>
2022-07-25 22:54:53 +0100

    change test suite name

--- portfolio/src/components/content/experience.test.js
+++ portfolio/src/components/content/experience.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Experience from './Experience';
 
-it('renders heading on page', () => {
+it('renders experience on page', () => {
   render(<Experience />);
   const linkElement = screen.getByTestId('experience-header');
   expect(linkElement).toHaveTextContent('Experience');
