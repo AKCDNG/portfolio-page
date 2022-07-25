fe0a9ee53319c44561b572d5fc62c467a56c24c0
Anish <anishkakaiya@outlook.com>
2022-07-25 21:52:39 +0100

    add test id

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <h1 className="titletest">Welcome To My Personal Portfolio!</h1>
+      <h1 data-testid='heading-portfolio' className="heading-portfolio">Welcome To My Personal Portfolio!</h1>
       <Navbar />
     </div>
   );
