69ff0b81159f11ead6da82ad47f8f41eec6048d4
Anish <anishkakaiya@outlook.com>
2022-07-25 23:11:55 +0100

    rename test

--- portfolio/src/components/content/projects.test.js
+++ portfolio/src/components/content/projects.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Projects from './Projects';
 
-it('renders experience on page', () => {
+it('renders projects on page', () => {
   render(<Projects />);
   const linkElement = screen.getByTestId('projects-header');
   expect(linkElement).toHaveTextContent('Projects');
