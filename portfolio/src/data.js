bcbd1df6cd2ca93130ff98d80bb98527e8b47c4b
Anish <anishkakaiya@outlook.com>
2022-07-26 15:48:38 +0100

    add data for links in navbar

---
+++ portfolio/src/data.js
@@ -0,0 +1,38 @@
+export const links = [
+  {
+    id: 1,
+    text: 'Welcome',
+    url: '#welcome'
+
+  },
+  {
+    id: 2,
+    text: 'About Me',
+    url: '#about-me'
+
+  },
+  {
+    id: 3,
+    text: 'Experience',
+    url: '#experience'
+
+  },
+  {
+    id: 4,
+    text: 'Projects',
+    url: '#projects'
+
+  },
+  {
+    id: 5,
+    text: 'Tech Stacks',
+    url: '#tech-stacks'
+
+  },
+  {
+    id: 6,
+    text: 'Contact',
+    url: '#contact'
+
+  }
+]
