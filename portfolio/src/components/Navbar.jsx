f798f41660b605959d2d69437c50a7cd660b618e
Anish <anishkakaiya@outlook.com>
2022-07-26 14:43:39 +0100

    add center div for navbar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
 const Navbar = () => {
   return (
-    <nav className="navbar" data-testid="navbar">
-      About Me
-      Experience
-      Projects
-      Tech Stacks
-      Contact
+    <nav className='navbar' data-testid='navbar'>
+      <div className='nav-center'>
+        About Me Experience Projects Tech Stacks Contact
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
