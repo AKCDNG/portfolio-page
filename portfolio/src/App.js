b944608c63cafe8a813cbd8bb70fa59c0c42afd2
Anish <anishkakaiya@outlook.com>
2022-07-26 21:59:21 +0100

    add container

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -11,25 +11,27 @@ function App() {
   return (
     <div className='App'>
       <Navbar />
-      <section className='welcome' id='welcome'>
-        <Welcome />
-      </section>
-      <section className='about-me' id='about-me'>
-        <AboutMe />
-      </section>
-      <section className='experience' id='experience'>
-        <Experience />
-      </section>
-      <section className='projects' id='projects'>
-        <Projects />
-      </section>
-      <section className='tech-stacks' id='tech-stacks'>
-        <TechStacks />
-      </section>
-      <section className='contact' id='contact'>
-        <Contact />
-      </section>
-      <section className='rest'></section>
+      <div id='container'>
+        <section className='welcome' id='welcome'>
+          <Welcome />
+        </section>
+        <section className='about-me' id='about-me'>
+          <AboutMe />
+        </section>
+        <section className='experience' id='experience'>
+          <Experience />
+        </section>
+        <section className='projects' id='projects'>
+          <Projects />
+        </section>
+        <section className='tech-stacks' id='tech-stacks'>
+          <TechStacks />
+        </section>
+        <section className='contact' id='contact'>
+          <Contact />
+        </section>
+        <section className='rest'></section>
+      </div>
     </div>
   );
 }
