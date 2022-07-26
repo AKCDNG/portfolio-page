baa208e421cf239a94199869ce59f5840a6851e0
Anish <anishkakaiya@outlook.com>
2022-07-26 14:42:45 +0100

    add main

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import './index.css';
 import Navbar from './components/Navbar';
-import Welcome from './components/content/Welcome'
+import Welcome from './components/content/Welcome';
 import AboutMe from './components/content/AboutMe';
 import Experience from './components/content/Experience';
 import Projects from './components/content/Projects';
@@ -9,27 +9,30 @@ import Contact from './components/content/Contact';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <section className="welcome">
-        <Welcome />
-      </section>
-      <section className='about-me'>
-        <AboutMe />
-      </section>
-      <section className='experience'>
-        <Experience />
-      </section>
-      <section className='projects'>
-        <Projects />
-      </section>
-      <section className='tech-stacks'>
-        <TechStacks />
-      </section>
-      <section className='contact'>
-        <Contact />
-      </section>
-    </div>
+    <main id='welcome'>
+      <div className='App'>
+        <Navbar />
+        <section className='welcome'>
+          <Welcome />
+        </section>
+        <section className='about-me' id='about-me'>
+          <AboutMe />
+        </section>
+        <section className='experience' id='experience'>
+          <Experience />
+        </section>
+        <section className='projects' id='projects'>
+          <Projects />
+        </section>
+        <section className='tech-stacks' id='tech-stacks'>
+          <TechStacks />
+        </section>
+        <section className='contact' id='contact'>
+          <Contact />
+        </section>
+        <section className='rest'></section>
+      </div>
+    </main>
   );
 }
 
