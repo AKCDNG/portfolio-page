1fa6db12ab07b52bc1cf83dd833584653f72fd07
Anish <anishkakaiya@outlook.com>
2022-07-27 14:42:07 +0100

    refactor and add p tag for footer

--- portfolio/src/components/footer/footer.jsx
+++ portfolio/src/components/footer/footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-  return <footer className='footer' data-testid='footer'>
-    <div className="footer-container">
-      Anish Kakaiya
-    </div>
-  </footer>;
+  return (
+    <footer className='footer' data-testid='footer'>
+      <div className='footer-container'>
+        <p className='footer-text'>Anish Kakaiya</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
